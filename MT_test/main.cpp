a9d5d185bd8721e2cf6e1f93d4719bfa3114a5f9
h3ll0kitt1 <52863098+h3ll0kitt1@users.noreply.github.com>
2020-12-22 13:45:03 +0300

    Add files via upload

---
+++ MT_test/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "mt19937 from <random> " <<std::endl;
+    std::mt19937 generator (5489);
+    for (int i = 0; i < 9; i++)
+    {
+        std::cout <<  generator() << " ";
+    }
+    std::cout << std::endl;
+}
